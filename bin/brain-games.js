36c8813fe8fb6dbc85328d7fbbd51815aef1929d
T.Rakhimov <t.rakhimov@MacBook-Pro-TRakhimov.local>
2021-06-14 23:15:11 +0300

    clean code

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 import {greeting} from '../src/cli.js';
-import {brain_even} from './brain-even.js';
 
 console.log('Welcome to the Brain Games!');
 const name = greeting();
-brain_even(name);
