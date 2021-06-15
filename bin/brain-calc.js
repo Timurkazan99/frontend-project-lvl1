63e5786ec037e2aa0a613dac4a2ff311c19cced2
T.Rakhimov <t.rakhimov@MacBook-Pro-TRakhimov.local>
2021-06-15 12:59:14 +0300

    fix

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -2,6 +2,7 @@
 
 import { greeting } from '../src/cli.js';
 import * as calc from '../games/brain-calc.js';
+import { game } from '../src/index.js';
 
 const name = greeting();
 game(calc.questions, name, calc.rule);
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,6 +2,7 @@
 
 import { greeting } from '../src/cli.js';
 import * as even from '../games/brain-even.js';
+import { game } from '../src/index.js';
 
 const name = greeting();
 game(even.questions, name, even.rule);
